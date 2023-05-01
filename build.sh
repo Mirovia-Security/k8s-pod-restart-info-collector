66fffe7c478074bc91943783a97a333352ae6836
Moshe Bas <moshe.bas@gmail.com>
2023-05-01 16:56:58 +0300

    create helm repo

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TAG="v1.2.1"
-docker buildx build --platform linux/amd64 -t devopsairwallex/k8s-pod-restart-info-collector:${TAG} .
-docker push devopsairwallex/k8s-pod-restart-info-collector:${TAG}
-
-docker tag devopsairwallex/k8s-pod-restart-info-collector:${TAG} devopsairwallex/k8s-pod-restart-info-collector:latest
-docker push devopsairwallex/k8s-pod-restart-info-collector:latest
